6a5dcef32c5a869dea147b55145122980d2cda2d
Ryan McCarthy & Tina Ramsey <student@epicodus.com>
2015-02-11 12:16:13 -0800

    Add fully functional coinCombo page with anatomically correct coin pngs

--- js/coin-combo-scripts.js
+++ js/coin-combo-scripts.js
@@ -15,3 +15,34 @@ var coinCombo = function(amount) {
   coinCount[3] = amount;
   return coinCount;
 };
+
+$(document).ready(function() {
+  $("form#amount").submit(function(event){
+    var amount = parseInt($("input#amount").val());
+    var coinCount = coinCombo(amount)
+
+    $(".amount").text(amount);
+    $("#result").show();
+    $("#coin-result").empty();
+    $("#coin-result").show();
+
+
+    for (i = coinCount[0]; i > 0; i--) {
+      $("#coin-result").append("<img id='quarter' src='css/img/quarter.png'>");
+    }
+
+    for (i = coinCount[1]; i > 0; i--) {
+      $("#coin-result").append("<img id='dime' src='css/img/dime.png'>");
+    }
+
+    for (i = coinCount[2]; i > 0; i--) {
+      $("#coin-result").append("<img id='nickel' src='css/img/nickel.png'>");
+    }
+
+    for (i = coinCount[3]; i > 0; i--) {
+      $("#coin-result").append("<img id='penny' src='css/img/penny.png'>");
+    }
+
+    event.preventDefault();
+  });
+});
